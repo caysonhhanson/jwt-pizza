cc0e2cf69524fdc7e8871d1d336a7816e78b0ea2
Cayson Hanson <chanballer27@gmail.com>
2025-02-11 16:19:40 -0700

    fixed the visiting lisit.

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -366,7 +366,7 @@ test('storeClosure', async ({ page }) => {
     }
   });
 
-  await page.goto('http://localhost:5174/');
+  await page.goto('/');
   await page.getByLabel('Global').getByRole('link', { name: 'Franchise' }).click();
   await page.getByRole('link', { name: 'login', exact: true }).click();
   
