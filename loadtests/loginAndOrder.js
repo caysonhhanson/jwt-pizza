757dc0d08c5f677d1772ac0d2d942cd5c39dfb42
Cayson Hanson <chanballer27@gmail.com>
2025-04-01 15:52:34 -0600

    Script

---
+++ loadtests/loginAndOrder.js
@@ -0,0 +1,180 @@
+import { sleep, group, check, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  const vars = {}
+
+  group('page_2 - https://pizza.cayson5.click/', function () {
+    response = http.get('https://pizza.cayson5.click/', {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        'if-modified-since': 'Wed, 12 Mar 2025 23:46:20 GMT',
+        'if-none-match': '"0225fcaea628b021806f4c735366820d"',
+        priority: 'u=0, i',
+        'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+    })
+    sleep(11.8)
+
+    response = http.put(
+      'https://pizza-service.cayson5.click/api/auth',
+      '{"email":"cayson@mail.com","password":"123456"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.cayson5.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+
+    // Add check for login success
+    if (!check(response, { 'login status is 200': r => r.status === 200 })) {
+      console.log(response.body);
+      fail('Login failed');
+    }
+
+    vars['token1'] = jsonpath.query(response.json(), '$.token')[0]
+
+    sleep(6.2)
+
+    response = http.get('https://pizza-service.cayson5.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token1']}`,
+        'content-type': 'application/json',
+        'if-none-match': 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+        origin: 'https://pizza.cayson5.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(0.5)
+
+    response = http.get('https://pizza-service.cayson5.click/api/franchise', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token1']}`,
+        'content-type': 'application/json',
+        'if-none-match': 'W/"40-EPPawbPn0KtYVCL5qBynMCqA1xo"',
+        origin: 'https://pizza.cayson5.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(20.6)
+
+    response = http.post(
+      'https://pizza-service.cayson5.click/api/order',
+      '{"items":[{"menuId":5,"description":"Charred Leopard","price":0.0099},{"menuId":2,"description":"Pepperoni","price":0.0042},{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.cayson5.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    
+    if (!check(response, { 'purchase status is 200': r => r.status === 200 })) {
+      console.log(response.body);
+      fail('Pizza purchase failed');
+    }
+    
+    vars['pizzaToken'] = jsonpath.query(response.json(), '$.jwt')[0]
+    
+    sleep(4.6)
+
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      `{"jwt":"${vars['pizzaToken']}"}`,
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.cayson5.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'sec-fetch-storage-access': 'active',
+        },
+      }
+    )
+  })
+}
